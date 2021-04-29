5c83155ce2ded92bd15b723c9613c0adcbd12e9e
aavinokurov <aavinokurov@outlook.com>
2021-04-29 22:58:45 +0300

    Create project

---
+++ HomeWork8/HomeWork8/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    public struct Worker
+    {
+        #region Поля
+
+
+
+        #endregion
+    }
+}
