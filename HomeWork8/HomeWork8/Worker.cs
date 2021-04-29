3b27e745f76f3f324c652787c17bb170e22bb71a
aavinokurov <aavinokurov@outlook.com>
2021-04-29 23:11:14 +0300

    Add class worker

--- HomeWork8/HomeWork8/Worker.cs
+++ HomeWork8/HomeWork8/Worker.cs
@@ -8,9 +8,60 @@ namespace HomeWork8
 {
     public struct Worker
     {
-        #region Поля
+        #region Свойства
 
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Surname { get; private set; }
 
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age { get; private set; }
+
+        /// <summary>
+        /// Департамент
+        /// </summary>
+        public Department Department { get; private set; }
+
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Размер заработной платы
+        /// </summary>
+        public float Salary { get; private set; }
+
+        #endregion
+
+        #region Конструкторы
+
+        /// <summary>
+        /// Создает нового работник
+        /// </summary>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="department">Департамент</param>
+        /// <param name="id">Идентификатор</param>
+        /// <param name="salary">Размер заработной платы</param>
+        public Worker(string surname, string name, int age, Department department, int id, int salary)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Age = age;
+            this.Department = department;
+            this.Id = id;
+            this.Salary = salary;
+        }
 
         #endregion
     }
